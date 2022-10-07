a7f98371910bcec2db1a67b4c3862a10fcacd7ff
joshua <joshuahsientan@gmail.com>
2022-10-07 12:58:41 -0500

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjectsJT
+{
+    internal class Circle
+    {
+        public double radius { get; set; }
+
+        //calculate C -> does the equation
+        //calculate formatted C -> creates the string that allows it to be more easily displayed. (see LINQ lab?)
+         //regular is raw equations
+         //formated is to call the methods for the results, and display them?
+         //( can also just output rounded numbers from one method, may not need formated)
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double CalculateCircumference(double radius)
+        {
+            double circumference = 2 * Math.PI * radius;
+            return Math.Round(circumference, 2);   //round to 2 decimal places
+        }
+
+        public double CalculateArea(double radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            return Math.Round(area, 2);   //round to 2 decimal places
+        }
+
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿namespace CircleObjectsJT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            int count = 0;
+
+            bool start = true;
+            while(start)
+            {
+                count++;
+                try //exception handling, accepts only numbers / decimals.
+                {
+                    Console.WriteLine("Enter the radius of a circle"); //prompt user to enter the number of sides for a pair of dice
+                    radius = double.Parse(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Your input was invalid, please enter a number");
+                    Console.WriteLine();
+                    continue;
+                }
+                Console.WriteLine();
+
+                if (radius <= 0)
+                {
+                    Console.WriteLine("Invalid input, you entered a number less than 0. Please enter a positive number or decimal.");
+                    Console.WriteLine();
+                }
+
+                Circle c = new Circle(radius);
+
+                //calc radius (round to 2 decimals)
+                Console.WriteLine($"The circumference is {c.CalculateCircumference(radius)}");
+
+                Console.WriteLine();
+
+                //calc area (round to 2 decimals)
+                Console.WriteLine($"The circumference is {c.CalculateArea(radius)}");
+                
+                Console.WriteLine();
+
+
+
+                Console.WriteLine("------------------------------------------------");
+                Console.WriteLine("Do you want to create another circle? Y/N");
+                string input = Console.ReadLine().ToLower();
+
+                if (input == "y")
+                {
+                    start = true;
+                }
+                else if (input == "n")
+                {
+                    
+                    Console.WriteLine($"Goodbye. You created {count} circles");
+                    break; 
+
+                }
+                else //if user input is not "y" or "n"
+                {
+                    Console.WriteLine("I'm sorry, I'm afraid I can't do that, invalid input. Please try again.");
+                    continue;
+                }
+            }
+        }
+    }
+}
